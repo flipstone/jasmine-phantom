604708e882ea47fde9723bcaaf924054eb828d06
Matt Campbell <matt@soupmatt.com>
2013-02-20 16:56:42 -0600

    support for jasmine 1.3

--- lib/jasmine-phantom/server.rb
+++ lib/jasmine-phantom/server.rb
@@ -6,11 +6,30 @@ module Jasmine
 
         case Jasmine::VERSION
         when '1.2.0' then start_1_2_0
+        when '1.2.1' then start_1_2_1
         else start_latest
         end
       end
 
       def self.start_latest
+        Jasmine.load_configuration_from_yaml
+        config = Jasmine.config
+
+        server = Jasmine::Server.new(config.port, Jasmine::Application.app(config))
+        t = Thread.new do
+          begin
+            server.start
+          rescue ChildProcess::TimeoutError
+          end
+          # ignore bad exits
+        end
+        t.abort_on_exception = true
+        Jasmine::wait_for_listener(config.port, "jasmine server")
+
+        config.port
+      end
+
+      def self.start_1_2_1
         jasmine_runner_config = Jasmine::RunnerConfig.new
         server = Jasmine::Server.new(jasmine_runner_config.port, Jasmine::Application.app(jasmine_runner_config))
 
